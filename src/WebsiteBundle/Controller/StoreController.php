b269fa0bbd93517bcd133a11d1406849532ec141
Yuri Cauna Robles <syslock64@gmail.com>
2017-09-01 17:56:40 -0500

    test step 4 culqi test charge

--- src/WebsiteBundle/Controller/StoreController.php
+++ src/WebsiteBundle/Controller/StoreController.php
@@ -17,6 +17,39 @@ class StoreController extends Controller
 
     public function chargeAction(Request $request)
     {
-        var_dump($request);die();
+        $token = $request->request->get('_t',null);
+        $mount = $request->request->get('_m',null);
+        $email = $request->request->get('_e',null);
+        $response = ['success'=>!1,'messages'];
+
+        if(empty($token) || empty($mount) || empty($email))
+        {
+            $response['messages'][] = 'Vuelve a procesar tu compra, ocurrio un error mientras se enviaron los datos.';
+            return new JsonResponse($response);
+        }
+
+        try{
+            $SECRET_KEY = "sk_test_0RIu4kUklwbEitlT";
+            $culqi = new Culqi(array('api_key' => $SECRET_KEY));
+
+            $charge = $culqi->Charges->create(
+                array(
+                    "amount" => $mount,
+                    "capture" => true,
+                    "currency_code" => "PEN",
+                    //"email" => $email,
+                    "description" => "Venta de prueba",
+                    "installments" => 0,
+                    "metadata" => array("test"=>"test"),
+                    "source_id" => $token
+                )
+            );
+            return  new JsonResponse($charge);
+        }
+        catch(\Exception $e)
+        {
+            var_dump($e->getMessage());
+        }
+
     }
 }
