851c34baaecf6d0eefe0541a7ca463206bf891f6
Yuri Cauna Robles <syslock64@gmail.com>
2017-09-01 17:29:40 -0500

    test step 4 culqi test charge

--- src/WebsiteBundle/Controller/StoreController.php
+++ src/WebsiteBundle/Controller/StoreController.php
@@ -2,7 +2,10 @@
 
 namespace WebsiteBundle\Controller;
 
+use Culqi\Culqi;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class StoreController extends Controller
 {
@@ -10,5 +13,41 @@ class StoreController extends Controller
     {
         return $this->render('WebsiteBundle:store:index.html.twig',[
         ]);
+    }
+
+    public function chargeAction(Request $request)
+    {
+        $token = $request->request->get('_t',null);
+        $mount = $request->request->get('_m',null);
+        $response = ['success'=>!1];
+
+        if(empty($token) || empty($mount))
+        {
+            $response['messages'][] = 'Vuelve a procesar tu compra, ocurrio un error mientras se enviaron los datos.';
+            return new JsonResponse($response);
+        }
+
+        try{
+            $SECRET_KEY = "sk_test_0RIu4kUklwbEitlT";
+            $culqi = new Culqi(array('api_key' => $SECRET_KEY));
+
+            $charge = $culqi->Charges->create(
+                array(
+                    "amount" => $mount,
+                    "capture" => true,
+                    "currency_code" => "PEN",
+                    "description" => "Venta de prueba",
+                    "installments" => 0,
+                    "metadata" => array("test"=>"test"),
+                    "source_id" => $token
+                )
+            );
+            return  new JsonResponse($charge);
+        }
+        catch(\Exception $e)
+        {
+            return new JsonResponse($e->getMessage());
+        }
+
     }
 }
